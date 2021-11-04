e358e9f5e30e7495d83f2ce40a9bf0829e1291c7
camilarociodiaz <camilarociodiaz@gmail.com>
2021-11-03 22:42:40 -0300

    2 entrega proyecto

---
+++ 2entregaProyectoFinal/segundaEntregaProyecto.js
@@ -0,0 +1,187 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#listaCarrito tbody');
+const vaciarCarrito = document.querySelector('#vaciarCarrito');
+const listaPrendas = document.querySelector('#listaPrendas');
+
+
+
+let articulosCarrito = [];
+
+//funcion para agregar al carrito una prenda cuando apretan el boton de agregar al carrito//
+cargarEvent();
+
+function cargarEvent() {
+
+    listaPrendas.addEventListener('click', agregarPrenda);
+
+
+
+    //para eliminar prendas del carrito
+
+    carrito.addEventListener('click', eliminarPrenda);
+
+    //vaciar el carrito
+
+    vaciarCarrito.addEventListener('click', () => {
+
+        articulosCarrito = [];
+
+        limpiarHTML();
+
+    });
+
+}
+
+function agregarPrenda(e) {
+
+    e.preventDefault();
+
+
+    if (e.target.classList.contains('agregarCarrito')) {
+        const prendaSeleccionada = e.target.parentElement.parentElement.parentElement;
+
+        leerDatosPrenda(prendaSeleccionada);
+
+    }
+}
+
+
+
+
+
+//eliminar prendas del carrito 
+function eliminarPrenda(e) {
+
+    e.preventDefault();
+
+    if (e.target.classList.contains('borrarPrenda')) {
+
+        console.log(e.target);
+
+
+        console.log(e.target.getAttribute('data-id'));
+
+        const prendaId = e.target.getAttribute('data-id');
+
+        articulosCarrito = articulosCarrito.filter(prenda => prenda.id !== prendaId);
+
+
+
+
+        carritoHTML();
+
+    }
+
+
+}
+
+
+
+
+function leerDatosPrenda(prenda) {
+
+
+    //creo un objeto con los datos de la prenda//
+
+    const infoPrenda = {
+        imagen: prenda.querySelector('img').src,
+        titulo: prenda.querySelector('h5').textContent,
+        precio: prenda.querySelector('p').textContent,
+        id: prenda.querySelector('a').getAttribute('data-id'),
+        cantidad: 1,
+
+    }
+
+
+    //revisar si un articulo ya existe en el carrito
+
+    const existe = articulosCarrito.some(prenda => prenda.id === infoPrenda.id);
+    if (existe) {
+
+        const prenda = articulosCarrito.map(prenda => {
+
+            if (prenda.id === infoPrenda.id) {
+
+                prenda.cantidad++;
+                return prenda;
+            } else {
+
+                return prenda;
+            }
+
+        });
+
+        articulosCarrito = [...prenda];
+
+    } else {
+
+        //agregar elementos al array carrito//
+
+        articulosCarrito = [...articulosCarrito, infoPrenda];
+    }
+
+
+
+
+
+
+
+
+
+
+    carritoHTML();
+
+}
+
+
+
+
+
+
+
+
+//mostrar el carrito de compras al HTML
+
+function carritoHTML() {
+
+    //limpiar HTML para evitar duplicados en el objeto
+
+    limpiarHTML();
+
+    //recorrer el carrito para generar el HTML
+
+    articulosCarrito.forEach(prenda => {
+
+        const rowCarrito = document.createElement('tr');
+        rowCarrito.innerHTML =
+
+            `<td>
+                    <img src="${prenda.imagen}" width="100">
+            </td>
+            <td>${prenda.titulo} </td>
+            <td> ${prenda.precio} </td>
+            <td> ${prenda.cantidad} </td>
+            <td> 
+            <a href='#' class="borrarPrenda" data-id${prenda.id}> x </a>
+            </td>      `
+
+
+        ;
+
+
+        //agrego el HTML del carrito al tbody
+
+        contenedorCarrito.appendChild(rowCarrito);
+
+
+    })
+
+}
+
+function limpiarHTML() {
+
+    while (contenedorCarrito.firstChild) {
+
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
